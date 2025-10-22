fd1f89336fb23841fc6ed1b0af4500a2553e18d4
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-10-22 14:53:37 +0100

    afficher candidats dans itemComponent

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Candidat } from '../models/candidat';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrl: './item.component.css'
+  styleUrl: './item.component.css',
 })
 export class ItemComponent {
-
+  @Input() unCandidat: Candidat;
 }
--- src/app/liste/liste.component.ts
+++ src/app/liste/liste.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Candidat } from '../models/candidat';
 
 @Component({
   selector: 'app-liste',
   templateUrl: './liste.component.html',
-  styleUrl: './liste.component.css'
+  styleUrl: './liste.component.css',
 })
 export class ListeComponent {
-
+  @Input() allCandidates: Candidat[] = [];
 }
