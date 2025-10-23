cfe157eaee48750091ffd335139a04b51308b0b5
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-10-23 10:11:43 +0100

    exercice manage server finalisé

--- src/app/manage-servers/manage-servers.component.ts
+++ src/app/manage-servers/manage-servers.component.ts
@@ -10,26 +10,34 @@ export class ManageServersComponent {
     {
       name: 'Production server',
       type: 'medium',
-      date_d: new Date(4, 6, 2020),
+      date_d: new Date(2020, 4, 6),
       status: 'critical',
     },
     {
       name: 'Development Test server',
       type: 'large',
-      date_d: new Date(4, 2, 2023),
+      date_d: new Date(2023, 4, 2),
       status: 'stable',
     },
     {
       name: 'Development server',
       type: 'medium',
-      date_d: new Date(2, 2, 2025),
+      date_d: new Date(2025, 2, 2),
       status: 'offline',
     },
     {
       name: 'Nidhal server',
       type: 'large',
-      date_d: new Date(4, 6, 2020),
+      date_d: new Date(2020, 4, 6),
       status: 'stable',
     },
   ];
+
+  affecterClasse(st) {
+    return {
+      'list-group-item-success': st == 'stable',
+      'list-group-item-danger': st == 'critical',
+      'list-group-item-warning': st == 'warning',
+    };
+  }
 }
